9529ba9a78727c83943e8cd3b4f0ea870289029f
Vandopal <petya-pavlov-200001@mail.ru>
2023-07-27 21:43:02 +0300

    finding a problem

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,50 +1,30 @@
-import { beforeEach, test, expect } from '@jest/globals';
+/* eslint-disable no-underscore-dangle */
+import { test, expect, describe } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src/index.js';
 
-import { execFileSync } from 'child_process';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import json from '../__fixtures__/json.expected.js';
-import plain from '../__fixtures__/plain.expected.js';
-import stylish from '../__fixtures__/stylish.expected.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let node1;
-let node2;
-let node3;
+const tests = [
+  ['filepath1.json', 'filepath2.json', 'stylish.expected.txt'],
+  ['filepath1.json', 'filepath2.json', 'stylish.expected.txt', 'stylish'],
+  ['filepath1.yml', 'filepath2.yml', 'stylish.expected.txt', 'stylish'],
+  ['filepath1.json', 'filepath2.json', 'plain.expected.txt', 'plain'],
+  ['filepath1.json', 'filepath2.json', 'json.expected.txt', 'json'],
+];
 
-beforeEach(() => {
-  const options = { encoding: 'utf8', cwd: path.join(dirname, '..') };
-
-  const result1 = execFileSync(
-    'node',
-    ['bin/gendiff.js', '--format', 'json', '__fixtures__/filepath1.json', '__fixtures__/filepath2.json'],
-    options,
-  );
-  const result2 = execFileSync(
-    'node',
-    ['bin/gendiff.js', '--format', 'stylish', '__fixtures__/filepath1.json', '__fixtures__/filepath2.json'],
-    options,
-  );
-  const result3 = execFileSync(
-    'node',
-    ['bin/gendiff.js', '--format', 'plain', '__fixtures__/filepath1.json', '__fixtures__/filepath2.json'],
-    options,
-  );
-  node1 = result1;
-  node2 = result2;
-  node3 = result3;
-});
-
-test('json', () => {
-  expect(node1).toEqual(json);
-});
-
-test('stylish', () => {
-  expect(node2).toEqual(stylish);
-});
-
-test('plain', () => {
-  expect(node3).toEqual(plain);
+describe.each(tests)('Compare data', (data1, data2, expectedResult, format) => {
+  const firstData = getFixturePath(data1);
+  const secondData = getFixturePath(data2);
+  const finalResult = genDiff(firstData, secondData, format);
+  const expectedOutcome = readFile(expectedResult);
+  test(`test ${data1} and ${data2} with ${format} format to ${expectedResult}`, () => {
+    expect(finalResult).toEqual(expectedOutcome);
+  });
 });
