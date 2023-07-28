918d3b9035cfe8dc2ea699bcf2cdc00d81a9703b
Vandopal <petya-pavlov-200001@mail.ru>
2023-07-28 16:25:04 +0300

    plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,20 +4,18 @@ import _ from 'lodash';
 const mapping = {
   root: ({ children }, iter, path) => {
     const output = children.flatMap((node) => mapping[node.type](node, iter, [...path]));
-    const out = `${output}`.split(',').map((val) => `Property ${val}`);
-    return out.join('\n');
+    return output.join('\n');
   },
   nested: ({ key, children }, iter, path) => {
     const output = children.flatMap((node) => mapping[node.type](node, iter, [...path, key]));
-    return `${output.filter((val) => val !== '')}`;
+    return output.filter((val) => val !== '').join('\n');
   },
   added: (node, iter, path) => `'${[...path, node.key].join('.')}' was added with value: ${typeof node.value === 'object' ? '[complex value]' : typeof node.value === 'string' ? `'${node.value}'` : node.value}`,
   deleted: (node, iter, path) => `'${[...path, node.key].join('.')}' was removed`,
   unchanged: () => '',
   changed: (node, iter, path) => {
     const { key, value1, value2 } = node;
-    const data1 = `'${[...path, key].join('.')}' was updated. From ${typeof value1 === 'object' ? '[complex value]' : typeof value1 === 'string' ? `'${value1}'` : value1} to ${typeof value2 === 'string' ? `'${value2}'` : value2}`;
-    return data1;
+    return `'${[...path, key].join('.')}' was updated. From ${typeof value1 === 'object' ? '[complex value]' : typeof value1 === 'string' ? `'${value1}'` : value1} to ${typeof value2 === 'string' ? `'${value2}'` : typeof value2 === 'object' ? '[complex value]' : value2}`;
   },
 };
 
