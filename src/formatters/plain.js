336199f99c1952c73006e8b780575feaad1022df
root <root@WIN-ETHDOG054HU>
2023-09-23 00:28:43 +0300

    plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,29 +1,49 @@
-#!/usr/bin/end node
 import _ from 'lodash';
 
-const mapping = {
-  root: ({ children }, iter, path) => {
-    const output = children.flatMap((node) => mapping[node.type](node, iter, [...path]));
-    const out = `${output}`.split(',').map((val) => (val === '…' ? '' : `Property ${val}`));
-    return out.join('\n');
-  },
-  nested: ({ key, children }, iter, path) => {
-    const output = children.flatMap((node) => mapping[node.type](node, iter, [...path, key]));
-    return `${output.filter((val) => val !== '')}`;
-  },
-  added: (node, iter, path) => `'${[...path, node.key].join('.')}' was added with value: ${typeof node.value === 'object' ? '[complex value]' : typeof node.value === 'string' ? `'${node.value}'` : node.value}`,
-  deleted: (node, iter, path) => `'${[...path, node.key].join('.')}' was removed`,
-  unchanged: () => '',
-  changed: (node, iter, path) => {
-    const { key, value1, value2 } = node;
-    const data1 = `'${[...path, key].join('.')}' was updated. From ${value1 === null ? null : typeof value1 === 'object' ? '[complex value]' : typeof value1 === 'string' ? `'${value1}'` : value1} to ${value2 === null ? null : typeof value2 === 'object' ? '[complex value]' : typeof value2 === 'string' ? `'${value2}'` : value2}`;
-    return data1;
-  },
+const getPath = (nodeNames) => nodeNames.flat().join('.');
+
+const getFormattedValue = (value) => {
+  switch (typeof value) {
+    case 'object': {
+      return !value ? 'null' : '[complex value]';
+    }
+    case 'string': {
+      return `'${value}'`;
+    }
+    default: {
+      return `${value}`;
+    }
+  }
 };
 
-const renderTree = (ast) => {
-  const iter = (node, path) => mapping[node.type](node, iter, path);
-  return iter(ast, []);
-};
+export function makePlainDiff(tree) {
+  const iter = (node, path) => node.map((child) => {
+    const currentPath = getPath([path, child.key]);
+    switch (child.type) {
+      case 'nested': {
+        return iter(child.children, currentPath);
+      }
+      case 'added': {
+        return `Property '${currentPath}' was added with value: ${getFormattedValue(child.value)}`;
+      }
+      case 'removed': {
+        return `Property '${currentPath}' was removed`;
+      }
+      case 'changed': {
+        return `Property '${currentPath}' was updated. From ${getFormattedValue(child.oldValue)} to ${getFormattedValue(child.newValue)}`;
+      }
+      case 'unchanged': {
+        return null;
+      }
+      default: {
+        throw Error('Uncorrect data');
+      }
+    }
+  });
+  return iter(tree.children, []);
+}
 
-export default renderTree;
+export default function makePlain(data) {
+  const result = makePlainDiff(data);
+  return _.flattenDeep(result).filter((el) => el).join('\n');
+}
